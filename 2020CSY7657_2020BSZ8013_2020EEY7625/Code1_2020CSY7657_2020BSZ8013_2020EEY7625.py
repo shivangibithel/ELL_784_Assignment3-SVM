f574d7a043d18464c325d0f387b36cd710f59a8f
Yukti Kaura <yuktikaura1@gmail.com>
2021-05-21 11:15:05 +0530

    Update Code1_2020CSY7657_2020BSZ8013_2020EEY7625.py

--- 2020CSY7657_2020BSZ8013_2020EEY7625/Code1_2020CSY7657_2020BSZ8013_2020EEY7625.py
+++ 2020CSY7657_2020BSZ8013_2020EEY7625/Code1_2020CSY7657_2020BSZ8013_2020EEY7625.py
@@ -142,14 +142,15 @@ def find_poly_degree(reg_type):
     #Identifying fit of the model
     print("MSE ", mse_mat_train, mse_mat_test )
     # Please uncomment to visualize.Commenting so that the code execution is not stopped for the want of showing plots
-    if(reg_type == 0):
-        plt.plot(np.log10(mse_mat_train[:,:]), color='LightSlateGray', label='Train Error')
-        plt.plot(np.log10(mse_mat_test[:,:]), color='MediumVioletRed', label='Test Error')
-        plt.xlabel('Degree')
-    else:
-        plt.plot(np.log10(mse_mat_train[deg, :]), color='LightSlateGray', label='Train Error')
-        plt.plot(np.log10(mse_mat_test[deg, :]), color='MediumVioletRed', label='Test Error')
-        plt.xlabel('Lambda index in range [1e-3[0], 1e-2[1], 1e-1[2], 1[3], 10[4], 100[5]]')
+    # if(reg_type == 0):
+    #     plt.plot(np.log10(mse_mat_train[:,:]), color='LightSlateGray', label='Train Error')
+    #     plt.plot(np.log10(mse_mat_test[:,:]), color='MediumVioletRed', label='Test Error')
+    #     plt.xlabel('Degree')
+    # else:
+    #     plt.plot(np.log10(mse_mat_train[deg, :]), color='LightSlateGray', label='Train Error')
+    #     plt.plot(np.log10(mse_mat_test[deg, :]), color='MediumVioletRed', label='Test Error')
+    #     plt.xlabel('Lambda index in range [1e-3[0], 1e-2[1], 1e-1[2], 1[3], 10[4], 100[5]]')
+
     # plt.ylabel('Error')
     # plt.legend(loc="upper right")
     # plt.show()
